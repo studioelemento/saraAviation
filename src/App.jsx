cf04c514429e8bffb2556ae1293d4f100b1a53da
Muhammed Niyas <niyashussain10@gmail.com>
2026-07-29 16:26:12 +0530

    false to true (maintnce)

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const ScrollToTop = () => {
 };
 
 function App() {
-  const isMaintenanceMode = false; // Set this to false to disable maintenance mode
+  const isMaintenanceMode = true; // Set this to false to disable maintenance mode
 
   if (isMaintenanceMode) {
     return <Maintenance />;
